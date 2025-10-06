49a2443e426aa1d7a97273a457db82052bad569b
Arun Kannan <arunkannan1701@gmail.com>
2025-10-06 12:40:28 +0530

    google auth account changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./contexts/authContext";
 
-const GOOGLE_CLIENT_ID = "650325829231-ibjms981gof9t5h4tj19ikg2r0kpvkms.apps.googleusercontent.com";// replace with your actual client ID
+const GOOGLE_CLIENT_ID = "515268703203-o83dfbc2p62qeek11id94nesc41le4h6.apps.googleusercontent.com";// replace with your actual client ID
 
 function App() {
   return (
